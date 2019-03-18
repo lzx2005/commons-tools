d5d9a5afdce13f593c783ac4530fc68b8f4aa445
Zhengxian Li <crow2005@vip.qq.com>
2019-03-18 13:47:22 +0800

    edit maxheap

--- src/main/java/com/lzx2005/sort/MaxHeap.java
+++ src/main/java/com/lzx2005/sort/MaxHeap.java
@@ -2,6 +2,9 @@ package com.lzx2005.sort;
 
 import com.lzx2005.common.CommonUtils;
 
+import java.util.Arrays;
+import java.util.Random;
+
 
 public class MaxHeap {
 
@@ -17,13 +20,13 @@ public class MaxHeap {
 
     public void insert(int num) {
         if (count >= capacity) throw new IndexOutOfBoundsException("heap is full");
-        this.data[++count] = num;
-        this.shiftUp(count);
+        data[++count] = num;
+        shiftUp(count);
     }
 
     private void shiftUp(int index) {
-        while (index > 1 && this.data[index] > this.data[index / 2]) {
-            CommonUtils.swap(this.data, index, index / 2);
+        while (index > 1 && data[index] > data[index / 2]) {
+            CommonUtils.swap(data, index, index / 2);
             index /= 2;
         }
     }
@@ -34,5 +37,30 @@ public class MaxHeap {
 
     public boolean isEmpty() {
         return count == 0;
+    }
+
+    public void print() {
+        for (int i : data) {
+            System.out.println(i + " ");
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if (data != null) {
+            Arrays.stream(data).forEach(i -> sb.append(i).append(","));
+            sb.deleteCharAt(sb.length()-1);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        MaxHeap maxHeap = new MaxHeap(11);
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            maxHeap.insert(random.nextInt(1000));
+        }
+        System.out.println(maxHeap);
     }
 }
