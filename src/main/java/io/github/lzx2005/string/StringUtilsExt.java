08fe9db6650c41cc44c4c4a55c0bbba6319743df
hzlizx <978744976@qq.com>
2019-03-21 18:49:37 +0800

    to camel case

--- src/main/java/io/github/lzx2005/string/StringUtilsExt.java
+++ src/main/java/io/github/lzx2005/string/StringUtilsExt.java
@@ -1,6 +1,5 @@
 package io.github.lzx2005.string;
 
-import io.github.lzx2005.common.CommonUtils;
 import org.apache.commons.lang.StringUtils;
 
 /**
@@ -41,5 +40,42 @@ public class StringUtilsExt {
         return true;
     }
 
+    /**
+     * 下划线转驼峰
+     *
+     * @param str   下划线字符串
+     * @return      转换后的字符串
+     */
+    public static String underLineToCamelCase(String str) {
+        return StringUtilsExt.toCamelCase(str, '_');
+    }
 
+    /**
+     * 中划线转驼峰
+     * @param str   中划线字符串
+     * @return      转换后的字符串
+     */
+    public static String midLineToCamelCase(String str) {
+        return StringUtilsExt.toCamelCase(str, '-');
+    }
+
+    private static String toCamelCase(String str, char c) {
+        if (StringUtils.isEmpty(str)) {
+            throw new NullPointerException("string is null");
+        }
+        char[] chars = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        boolean nextUpper = false;
+        for (char aChar : chars) {
+            if (aChar == c) {
+                nextUpper = true;
+                continue;
+            } else if (nextUpper) {
+                aChar -= 32;
+                nextUpper = false;
+            }
+            sb.append(aChar);
+        }
+        return sb.toString();
+    }
 }
